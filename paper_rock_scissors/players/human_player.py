8f855b9d401861ad1c0acb65cbc8ca926b616c09
vcabral19 <vitorcabral19@gmail.com>
2022-05-21 15:33:51 +0200

    convert user input to enum

--- paper_rock_scissors/players/human_player.py
+++ paper_rock_scissors/players/human_player.py
@@ -5,8 +5,9 @@ from paper_rock_scissors.symbol import Symbol
 class HumanPlayer(Player):
 
     def play(self) -> Symbol:
-        symbol = input("Enter the number for the symbol of this match:"
-                       f"{Symbol.PAPER.name}: [{Symbol.PAPER.value}]"
-                       f"{Symbol.ROCK.name}: [{Symbol.ROCK.value}]"
-                       f"{Symbol.SCISSORS.name}: [{Symbol.SCISSORS.value}]")
+        user_input = input("Enter the number for the symbol of this match:"
+                           f"{Symbol.PAPER.name}: [{Symbol.PAPER.value}]"
+                           f"{Symbol.ROCK.name}: [{Symbol.ROCK.value}]"
+                           f"{Symbol.SCISSORS.name}: [{Symbol.SCISSORS.value}]")
+        symbol = Symbol(user_input)
         return symbol
